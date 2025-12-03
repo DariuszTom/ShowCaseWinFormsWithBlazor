e9fe6d5a1f684dd22f500b0cf73512c316e075f3
almos <almos@DESKTOP-T1TVG1I>
2025-12-03 02:49:49 +0100

    Cleaning repository

--- ShowCaseWeb/Program.cs
+++ ShowCaseWeb/Program.cs
@@ -1,13 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 using ShowCaseShared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
+builder.Services.AddRazorPages();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
 builder.Services.AddSingleton<CounterService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,9 +18,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+///
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+// Anti-forgery must be registered in the pipeline after authentication/authorization and before endpoints
 app.UseAntiforgery();
 
 app.MapStaticAssets();
